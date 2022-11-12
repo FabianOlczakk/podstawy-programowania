c22e05072782e84bbd22bc5e7e271b7cef0ba6e3
FabianOlczakk <fabianolczak22@gmail.com>
2022-11-12 21:05:31 +0100

    zad1d

---
+++ lista2/zad1d.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+
+    list<float> numbers;
+    int n;
+
+    for (int i = 1; i < 11; i++) {
+        cout << "Podaj liczbe " << i << ": ";
+        cin >> n;
+        numbers.push_back(n);
+    }
+
+    int sum = 0;
+    int max = numbers.front();
+    int min = numbers.front();
+    for (int i : numbers) {
+        cout << i << "\n";
+        sum += i;
+
+        if (i > max) max = i;
+        if (i < min) min = i;
+    }
+
+    cout << "Srednia: " << (float)sum / 10 << "\n";
+    cout << "Suma: " << sum << "\n";
+    cout << "Max: " << max << "\n";
+    cout << "Min: " << min << "\n";
+
+    return 0;
+}
